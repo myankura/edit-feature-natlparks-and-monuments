48ed6ab845a34bf2d9c66627facb5cf2e662d697
Michael Yankura <myankura210@gmail.com>
2019-03-26 08:38:43 -0500

    edit feature is working, radio buttons are picking up input, changes are being committed to json.

--- src/scripts/park.js
+++ src/scripts/park.js
@@ -7,6 +7,9 @@ const buildParkHtml = parkObject => {
   const parkArticle = buildElement("article", `national-park--${parkObject.id}`);
   parkArticle.appendChild(buildElement("h3", undefined, parkObject.name));
   parkArticle.appendChild(buildElement("p", undefined, parkObject.state));
+  // parkArticle.appendChild(buildElement("p", undefined, parkObject.latitude));
+  // parkArticle.appendChild(buildElement("p", undefined, parkObject.longitude))
+  parkArticle.appendChild(buildElement("p", undefined, "Visited: " + parkObject.visited))
 
   if(parkObject.visited !== true) {
     let visitedParkButton = buildElement("button", undefined, "Visited Park")
@@ -26,13 +29,38 @@ const buildParkHtml = parkObject => {
 
 const parkEditForm = (parkObject) => {
   let editFormFragment = document.createDocumentFragment()
-
+  //edit park name feature
   editFormFragment.appendChild(buildElement("label", undefined, "Name: "))
   editFormFragment.appendChild(buildElement("input", `edit-park-name--${parkObject.id}`, undefined, parkObject.name))
-
+  //edit park state feature
   editFormFragment.appendChild(buildElement("label", undefined, "State: "))
   editFormFragment.appendChild(buildElement("input", `edit-park-state--${parkObject.id}`, undefined, parkObject.state))
+  //edit park latitude feature
+  editFormFragment.appendChild(buildElement("label", undefined, "Latitude: "))
+  editFormFragment.appendChild(buildElement("input", `edit-park-lat--${parkObject.id}`, parkObject.latitude))
+  //edit park longitude feature
+  editFormFragment.appendChild(buildElement("label", undefined, "Longitude: "))
+  editFormFragment.appendChild(buildElement("input", `edit-park-long--${parkObject.id}`, parkObject.longitude))
+  //update visited feature
+  //visited
+  editFormFragment.appendChild(buildElement("label", undefined, "Visited: "))
+  let visitedTrueRadioBtn = document.createElement("input");
+  visitedTrueRadioBtn.type = "radio";
+  visitedTrueRadioBtn.name = "visited";
+  visitedTrueRadioBtn.id = `edit-visited--${parkObject.id}`;
+  visitedTrueRadioBtn.value = true;
+  visitedTrueRadioBtn.checked = true;
+  editFormFragment.appendChild(visitedTrueRadioBtn);
+  //not visited
+  editFormFragment.appendChild(buildElement("label", undefined, "Not Visited: "))
+  let visitedFalseRadioBtn = document.createElement("input");
+  visitedFalseRadioBtn.type = "radio";
+  visitedFalseRadioBtn.name = "visited";
+  visitedFalseRadioBtn.value = false;
+  visitedFalseRadioBtn.checked = true;
+  editFormFragment.appendChild(visitedFalseRadioBtn);
 
+  //update button feature
   const updateParkButton = buildElement("button", undefined, "Update")
   updateParkButton.addEventListener("click", handleUpdate)
   editFormFragment.appendChild(updateParkButton)
